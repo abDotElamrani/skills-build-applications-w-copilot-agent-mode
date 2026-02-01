839d2138d32a16ebbc5e8d59afc42aca87022005
Abdessamad EL AMRANI <162600168+abDotElamrani@users.noreply.github.com>
2026-02-01 14:25:13 +0000

    feat: configurer ALLOWED_HOSTS et ajuster l'URL de l'API pour les environnements de développement

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-!x9g7l#!iguljk$l$$$hu6e*(edl*=eaz4ely31inq0vckiv_*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
